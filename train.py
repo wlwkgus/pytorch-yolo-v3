40f6d1812d16314fb2d2a035faffeae74841017a
Tony Kim <wlwkgus1994@gmail.com>
2018-05-21 12:10:44 +0900

    single image visdom

--- train.py
+++ train.py
@@ -12,6 +12,7 @@ import os.path as osp
 from darknet import Darknet
 from preprocess import prep_image, inp_to_image
 import visdom
+import cv2
 
 
 def arg_parse():
@@ -68,6 +69,9 @@ class PlotManager(object):
                 'xlabel': 'step',
                 'ylabel': 'loss'},
             win=self.display_id)
+
+    def plot_image(self, image_numpy, title):
+        self.vis.image(image_numpy, opts=dict(title=title), win=self.display_id + 1)
 
 
 if __name__ == '__main__':
@@ -239,6 +243,22 @@ if __name__ == '__main__':
                     ('batch_loss', total_loss.numpy()[0]),
                     ('none', 0.)
                 ]))
+                color = (80, 7, 65)
+                single_prediction = prediction[0]
+                single_image_cv_format = (batch['img'][0].numpy() * 255).astype(np.int32).transpose(1, 2, 0)
+                single_image_cv_format = single_image_cv_format[..., ::-1]
+                for j in range(single_prediction.size(0)):
+                    if single_prediction[j][4] < 0.5:
+                        continue
+                    x_center, y_center, w, h = tuple(single_prediction[j][0:4].int())
+                    cv2.rectangle(
+                        single_image_cv_format,
+                        (int(x_center - w / 2), int(y_center - h / 2)),
+                        (int(x_center + w / 2), int(y_center + h / 2)),
+                        color,
+                        1
+                    )
+                plot_manager.plot_image(single_image_cv_format[..., ::-1], 'sample_image')
 
         save_filename = 'yolo_net' + '-' + str(epoch)
         save_path = os.path.join('ckpt', save_filename)
